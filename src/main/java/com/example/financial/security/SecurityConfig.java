d4b96fab3f76150da87187e17f01ace9bbfdd4d1
dueiriel <40749364+dueiriel@users.noreply.github.com>
2026-01-27 18:41:17 -0300

    Atualiza SecurityConfig para incluir suporte a CORS

--- src/main/java/com/example/financial/security/SecurityConfig.java
+++ src/main/java/com/example/financial/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4042"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4042", "http://localhost:3000", "http://localhost:8081"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
